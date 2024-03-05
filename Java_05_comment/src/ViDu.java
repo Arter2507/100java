0a4c4171fdebe2cdd2e96ef021c5aa7c6fe1ba54
Quang Tuong <qtuong.257@gmail.com>
2024-03-05 22:32:30 +0700

    Comment in Java

--- Java_05_comment/src/ViDu.java
+++ Java_05_comment/src/ViDu.java
@@ -1,4 +1,20 @@
-
+/**
+ * 
+ * @author TUONG
+ *
+ */
 public class ViDu {
-
+	/**
+	 * 
+	 * @param args: tham số đầu vào
+	 */
+	public static void main(String[] args) {
+		// Khai báo biến
+		int x = 10;
+		/*
+		 * Xuất số x ra màn hình sử dụng sysout
+		 * 
+		 */
+		System.out.println(x);
+	}
 }
