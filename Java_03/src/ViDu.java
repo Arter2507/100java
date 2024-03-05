24a20013b4cd43d1e3ac2c6ec56597aac80df702
Quang Tuong <qtuong.257@gmail.com>
2024-03-05 21:23:49 +0700

    Day 03 Data type in java

---
+++ Java_03/src/ViDu.java
@@ -0,0 +1,16 @@
+// Data type Example
+public class ViDu {
+	public static void main(String[] args) {
+		String bookName = "Lap Trinh Java";
+		int publishingYear = 2021;
+		double price = 15.5;
+		boolean available = true;
+		char repositoryCode = 'M';
+		
+		System.out.println("Book's Name: " + bookName);
+		System.out.println("Year of publication: " + publishingYear);
+		System.out.println("Price: " + price);
+		System.out.println("Available: " + available);
+		System.out.println("Repository Code: " + repositoryCode);
+	}
+}
