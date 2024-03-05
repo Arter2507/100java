004b9bfe180bea5b13a4c3c923f24e29516173e5
Quang Tuong <qtuong.257@gmail.com>
2024-03-05 21:23:22 +0700

    Day 02 variables and declaring variables in java

---
+++ Java_02/src/ViDu.java
@@ -0,0 +1,14 @@
+//Declaring Variables Examle
+
+public class ViDu {
+	public static void main(String[] args) {
+		String notification = "Xin chao";
+		String fullName = " To Quang Tuong";
+		String bookName;
+		bookName = "Lap Trinh Java";
+		
+		System.out.println("Notice: " + notification);
+		System.out.println("Full Name: " + fullName);
+		System.out.println("Name's Book: " + bookName);
+	}
+}
