562b5d8e9e2a47f12aebda2a826272bf66e1a4a7
Quang Tuong <qtuong.257@gmail.com>
2024-03-05 21:22:37 +0700

    Day 01 structure of java class

---
+++ Java_01_StructureClass/src/Hello.java
@@ -0,0 +1,9 @@
+//Structure of Java Class
+
+public class Hello {
+	// main => ctrl + space => enter
+	public static void main(String[] args) {
+	// sysout => ctrl + space => enter
+		System.out.println("Hello Word!!!");
+	}
+}
