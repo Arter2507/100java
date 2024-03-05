5c0c41a1a5da5ac4a4f8b768c152b521b8cbb7fe
Quang Tuong <qtuong.257@gmail.com>
2024-03-05 22:30:51 +0700

    Constants in Java

---
+++ Java_04/src/ViDu.java
@@ -0,0 +1,10 @@
+// Constant in Java
+public class ViDu {
+	public static void main(String[] args) {
+		final int number = 100;
+		
+		System.out.println("Number = " + number);
+		
+//		number = 50; // error
+	}
+}
