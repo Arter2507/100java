f6b4266e84267c92e146cd3466413131ee9d5ec7
Quang Tuong <qtuong.257@gmail.com>
2024-03-20 21:22:50 +0700

    How to assign data in java

---
+++ Java_10/src/ViDu.java
@@ -0,0 +1,31 @@
+// Assign data in java
+
+import java.util.Scanner;
+
+public class ViDu {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		float a;
+		System.out.println("Enter value a = ");
+		a = sc.nextFloat();
+
+		// Assign
+		System.out.println("a = " + a);
+		
+		a += 3; // a = a + 3;
+		System.out.println("a+=3: " + a);
+		
+		a -= 2; // a = a - 2;
+		System.out.println("a-=2: " + a);
+		
+		a *= 3; // a = a * 3;
+		System.out.println("a*=3: " + a);
+		
+		a /= 3; // a = a / 3;
+		System.out.println("a/=3: " + a);
+		
+		a %= 3; // a = a % 3;
+		System.out.println("a%=3: " + a);
+
+	}
+}
